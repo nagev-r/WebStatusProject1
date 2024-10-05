b9ff9634adc227042387516ac735abaea07019b4
justindll <justindelaluz7@gmail.com>
2024-10-05 15:39:04 -0400

    moved socket and http setup into for loop

--- monitor.py
+++ monitor.py
@@ -42,30 +42,28 @@ for url in urls:
     except Exception as e:
         print(f'Error parsing URL {url}: {e}')
 
-"""
-sock = None
-# create client socket, connect to server
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(5)
-    sock.connect((host, port))
-except Exception as e:
-    print(f'Network Error:\n {e}')
+    # create client socket, connect to server
+    sock = None
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect((host, port))
+    except Exception as e:
+        print(f'Network Error:\n {e}')
 
-if sock:
-    # send http request
-    request = f'GET {path} HTTP/1.0\r\n'
-    request += f'Host: {host}\r\n'
-    request += '\r\n'
-    sock.send(bytes(request, 'utf-8'))
+    if sock:
+        # send http request
+        request = f'GET {path} HTTP/1.0\r\n'
+        request += f'Host: {host}\r\n'
+        request += '\r\n'
+        sock.send(bytes(request, 'utf-8'))
 
-    # receive http response
-    response = b''
-    while True:
-        data = sock.recv(4096)
-        response += data
-        if not data:
-            break
-    print(response.decode('utf-8'))
-    sock.close()
-"""
+        # receive http response
+        response = b''
+        while True:
+            data = sock.recv(4096)
+            response += data
+            if not data:
+                break
+        print(response.decode('utf-8'))
+        sock.close()
