1b0c6f7fc72ed5c4ba16ad004961e8bb23e35552
justindll <justindelaluz7@gmail.com>
2024-10-05 17:56:08 -0400

    Consolidated web monitor code into its own function, making our code more reusable.
    Added Exception handling in case of response errors.

--- monitor.py
+++ monitor.py
@@ -3,6 +3,68 @@ import socket
 import ssl #module for secure communication with https
 from urllib.parse import urlparse
 
+def web_monitor(url):
+
+    try:
+        #Parsing
+        parsed_url = urlparse(url)
+
+        # server, port, and path should be parsed from url
+        host = parsed_url.hostname
+        path = parsed_url.path if parsed_url.path else '/'
+
+        #change port dependent on scheme
+        if parsed_url.scheme == 'https':
+            port = 443
+        else:
+            port = 80
+        #print(f'{url}\n')
+        #print(f'{host}\n')
+        #print(f'{path}\n')
+        #print(f'{port}\n')
+    except Exception as e:
+        print(f'Error parsing URL {url}:\n{e}\n')
+        return #exit function after exception
+
+    # create client socket, connect to server
+    sock = None
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect((host, port))
+
+        #If URL contains HTTPS, wrap socket with SSL
+        if parsed_url.scheme == 'https':
+            #SSL context creation
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT) #Protocol
+            context.load_default_certs() #Certs
+            sock = context.wrap_socket(sock, server_hostname=host) #Wrap sock for HTTPS
+
+    except Exception as e:
+        print(f'Network Error while connecting to {url}:\n{e}\n')
+        return #Skip to next url if exception occurs
+
+    if sock: #If an error occurs and no sock variable is created, the program will move onto the next url
+        try:
+            # send http request
+            request = f'GET {path} HTTP/1.0\r\n'
+            request += f'Host: {host}\r\n'
+            request += '\r\n'
+            sock.send(bytes(request, 'utf-8'))
+
+            # receive http response
+            response = b''
+            while True:
+                data = sock.recv(4096)
+                response += data
+                if not data:
+                    break
+            print(f'Response from {url}:\n{response.decode("utf-8")}\n')
+        except Exception as e:
+            print(f'Error receiving response from {url}:\n{e}\n')
+        finally:
+            sock.close()
+        
 # get urls_file name from command line
 if len(sys.argv) != 2:
     print('Usage: monitor urls_file')
@@ -21,60 +83,4 @@ except FileNotFoundError:
 
 for url in urls:
     url = url.strip()
-
-    try:
-        #Parsing
-        parsed_url = urlparse(url)
-
-
-        # server, port, and path should be parsed from url
-        host = parsed_url.hostname
-        path = parsed_url.path if parsed_url.path else '/'
-
-        #change port dependent on scheme
-        if parsed_url.scheme == 'https':
-            port = 443
-        else:
-            port = 80
-        #print(f'{url}\n')
-        #print(f'{host}\n')
-        #print(f'{path}\n')
-        #print(f'{port}\n')
-    except Exception as e:
-        print(f'Error parsing URL {url}:\n{e}\n')
-        continue
-
-    # create client socket, connect to server
-    sock = None
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(5)
-        sock.connect((host, port))
-
-        #If URL contains HTTPS, wrap socket with SSL
-        if parsed_url.scheme == 'https':
-            #SSL context creation
-            context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT) #Protocol
-            context.load_default_certs() #Certs
-            sock = context.wrap_socket(sock, server_hostname=host) #Wrap sock for HTTPS
-
-    except Exception as e:
-        print(f'Network Error while connecting to {url}:\n{e}\n')
-        continue #Skip to next url if exception occurs
-
-    if sock: #If an error occurs and no sock variable is created, the program will move onto the next url
-        # send http request
-        request = f'GET {path} HTTP/1.0\r\n'
-        request += f'Host: {host}\r\n'
-        request += '\r\n'
-        sock.send(bytes(request, 'utf-8'))
-
-        # receive http response
-        response = b''
-        while True:
-            data = sock.recv(4096)
-            response += data
-            if not data:
-                break
-        print(f'Response from {url}:\n{response.decode("utf-8")}\n')
-        sock.close()
+    web_monitor(url)
